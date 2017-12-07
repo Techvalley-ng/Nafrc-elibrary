c4ac5cd889550a9609c4d44249bef00aad31b712
abubakari bakari <bakari607@gmail.com>
2017-12-07 11:18:31 +0000

    login face face one

--- index.php
+++ index.php
@@ -1,3 +1,66 @@
-<?php
-    echo "hello";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/fam.css">
+        <link rel="stylesheet" href="css/elibrary.css" type="text/css" />
+        <title>NAFRC E-LIBRARY</title>
+    </head>
+    <body ng-app>
+        <!--top bar-->
+        <div class="container-fluid">
+            <div class="row">
+                <img src="/image/topbar.jpg" class="img-fluid" alt="Responsive image">
+            </div>
+        </div>   
+     <!--end of the top bar-->
+     
+     <!--bana pictur-->
+      <div class="container-fluid">
+            <div class="row">
+                <img src="/image/bana.jpg" class="img-fluid" alt="Responsive image">
+            </div>
+        </div> 
+     <!--end of bana picture-->
+     
+         <!--portal name-->
+     <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <div class="text-center name">E-LIBRARY PORTAL</div>
+                </div>
+            </div>
+        </div> 
+    <!--end of portal name    -->
+    
+    <!--login box-->
+    <div class="container-fluid">
+            <div class="row justify-content-md-center">
+                <div class="col-md-3">
+                    <div class="login-box">
+                        
+                        <input type="text" class="form-control"  placeholder="Username">
+                        <input type="password" class="form-control" placeholder="Password">
+                        <button type="button" class="btn btn-primary form-control" style="margin-top:.5em;cursor: pointer;">Login</button>
+                        
+                    </div>
+                </div>
+            </div>
+        </div> 
+    <!--endo fo login box-->
+        
+    
+    
+   
+    
+    <script src="/js/jquery.js"></script>    
+    <script src="/js/popper.js"></script>
+    <script src="/js/angularjs/angular.js"></script> 
+    <script src="/js/angularjs/angularroute.js"></script>
+    <script src="/js/angularjs/angularuirouter.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/jqueryui.min.js"></script>
+    <script src="/js/shake.js"></script> 
+    <script src="js/elibrary.js"></script>
+    </body>
+</html>
